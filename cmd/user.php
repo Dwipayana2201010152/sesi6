ae31dde5b50ddbbb05c655ae96524dd5e4f4a88b
cloud <cloud@34.124.223.51>
2023-04-26 09:45:54 +0800

    first commit

---
+++ cmd/user.php
@@ -0,0 +1,20 @@
+<?php
+    include("database/koneksi.php");
+
+    function crateuser($nama, $email, $user, $pass){
+
+        $stt = false;
+        $iduser = md5($user);
+        $sql = "INSERT INTO tbuser(nama, email, username, passkey, iduser) 
+        VALUES(
+            '$nama',
+            '$email',
+            '$user',
+            '$pass',
+            '$iduser'
+
+        );";
+        echo "DEBUG: ".$sql;
+        return $stt;
+
+    }
